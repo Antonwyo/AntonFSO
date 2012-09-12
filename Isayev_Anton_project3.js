130e9e4a564dcbf69e20bbf3395b70e493333170
Anton Isayev <Antonwyo@fullsail.edu>
2012-09-12 17:01:53 -0600

    Add var and Json

---
+++ Isayev_Anton_project3.js
@@ -0,0 +1,34 @@
+//Isayev Anton 
+// Project 3
+// SDI 1209
+
+var myname = "Anton ";
+var myfriends = [
+				"Christian", 
+				"Curtis", 
+				"Val"
+				]; 
+var PC = 4 ;
+var games =  [
+			 " Left 4 Dead ",
+			 " Diablo 3 ",	
+		     " Serious Sam ",
+		     " Borderlands "
+		     ];
+var json = [
+			{
+			friend : " Curtis ",
+			playertype: " good blocking ",
+			role : " gunner " 
+			},
+			{
+			friend : " Val ",
+			playertype : " support ",
+			role : " medic "
+			},
+			{
+			friend : " Christian ",
+			playertype : " sharpshooter ",
+			role : " sniper "
+			}
+           ];
